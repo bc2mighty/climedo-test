1bd77d6bcdedc3e5beefe3d9e7c672547314fee1
bc2mighty <akinadetunmise8@gmail.com>
2021-05-08 14:59:22 +0100

    Changed Application start bash script file

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 777 /opt/
+sudo chmod -R 777 /opt/backend
 
 #navigate into our working directory where we have all our github files
 cd /opt/backend
